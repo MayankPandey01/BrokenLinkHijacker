1e748bacb734654e13764611091aed3e04fb3c81
David Balas <ballistadavid@gmail.com>
2022-05-24 20:49:35 +0300

    fixed number_of_broken_link count and is_valid function to check only http, https protocols

--- BLH.py
+++ BLH.py
@@ -9,10 +9,10 @@ import random
 
 colorama.init()
 
-headers = { 'user-agent' : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36"
- }
+headers = {'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36"
+           }
 
-to_verify_ssl_cert=False #Change it to True if SSL errors are thrown
+to_verify_ssl_cert = False  # Change it to True if SSL errors are thrown
 urllib3.disable_warnings()
 GREEN = colorama.Fore.GREEN
 GRAY = colorama.Fore.LIGHTBLACK_EX
@@ -22,26 +22,23 @@ BLUE = colorama.Fore.BLUE
 CYAN = colorama.Fore.CYAN
 
 
-social_list=["twitter.com","facebook.com","instagram.com","linkedin.com","youtube.com","twitch.com","twitch.tv","discord.com","slack.com","soundcloud.com","medium.com",
-"vimeo.com","skype.com","pinterest.com","ct.pinterest.com","snapchat.com","telegram","t.me","telegram.com","clickcease.com","wistia.com","adjust.com","github.com"
-
-]
+social_list = ["twitter.com", "facebook.com", "instagram.com", "linkedin.com", "youtube.com", "twitch.com", "twitch.tv", "discord.com", "slack.com", "soundcloud.com", "medium.com",
+               "vimeo.com", "skype.com", "pinterest.com", "ct.pinterest.com", "snapchat.com", "telegram", "t.me", "telegram.com", "clickcease.com", "wistia.com", "adjust.com", "github.com"]
 
 inbound_urls = set()
 outbound_urls = set()
-current_inbound_urls =set()
-broken_urls  = set()
+current_inbound_urls = set()
+broken_urls = set()
 social_urls = []
-number_of_broken_link=0
+number_of_broken_link = 0
 
 
 total_urls_visited = 0
 
 
-
 def banner():
-    version="1.0"
-    ascii_banner="""
+    version = "1.0"
+    ascii_banner = """
      ____            _                _     _       _
     | __ ) _ __ ___ | | _____ _ __   | |   (_)_ __ | | __
     |  _ \| '__/ _ \| |/ / _ \ '_ \  | |   | | '_ \| |/ /
@@ -56,43 +53,41 @@ def banner():
            |__/
     """
     print(ascii_banner)
-    print(f"{RED}                        Version-",version)
+    print(f"{RED}                        Version-", version)
+
+
 def random_ua():
-    UAS=("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36",
-     "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36",
-     "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36",
-     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36",
-     "Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1",
-     "Mozilla/5.0 (Linux; Android 10; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.181 Mobile Safari/537.36"
-     )
+    UAS = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36",
+           "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36",
+           "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36",
+           "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36",
+           "Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1",
+           "Mozilla/5.0 (Linux; Android 10; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.181 Mobile Safari/537.36"
+           )
 
-    ua = UAS[random.randrange(len(UAS))] # to generate random User-Agent for deep crawling
-    ua=str(ua)
-    headers['user-agent']=ua
+    # to generate random User-Agent for deep crawling
+    ua = UAS[random.randrange(len(UAS))]
+    ua = str(ua)
+    headers['user-agent'] = ua
 
-def is_valid(url): #Checking For Valid URL's
+
+def is_valid(url):  # Checking For Valid URL's
     try:
         parsed = urlparse(url)
-        return bool(parsed.netloc) and bool(parsed.scheme)
-        valid_request=requests.get(url,headers=headers,verify=to_verify_ssl_cert)
-        if valid_request!=200:
-            print(f"{RED} Enter Valid Url {RESET} ")
-            sys.exit()
-            
-    except:
-        print(f"{RED} ERROR {RESET} ")
+        return bool(parsed.netloc) and bool(parsed.scheme) and parsed.scheme in ['http', 'https']
+    except Exception as e:
+        print(f"{RED} ERROR Parsing URL {url} {RESET}\n{e}")
         sys.exit()
 
 
-
-
-def main_webpage_links(url):   #Returns all URLs from the given Main Website
+def main_webpage_links(url):  # Returns all URLs from the given Main Website
     try:
         urls = set()
         random_ua()
         domain_name = urlparse(url).netloc
-        soup = BeautifulSoup(requests.get(url,headers=headers,verify=to_verify_ssl_cert).content, "html.parser")
-        for a_tag in soup.findAll("a"):              #CHECK FOR LINKS IN HERF TAG
+        soup = BeautifulSoup(requests.get(
+            url, headers=headers, verify=to_verify_ssl_cert).content, "html.parser")
+        for a_tag in soup.findAll("a"):  # CHECK FOR LINKS IN HERF TAG
             href = a_tag.attrs.get("href")
             if href == "" or href is None:
                 continue
@@ -108,7 +103,7 @@ def main_webpage_links(url):   #Returns all URLs from the given Main Website
                     if verbosity:
                         print(f"{BLUE}[!] Outbound link: {href}{RESET}")
                     outbound_urls.add(href)
-                    social_domain=str(urlparse(href).netloc)
+                    social_domain = str(urlparse(href).netloc)
                     if social_domain and social_domain.strip('www.') in social_list:
                         social_urls.append(href)
                 continue
@@ -116,8 +111,8 @@ def main_webpage_links(url):   #Returns all URLs from the given Main Website
                 print(f"{GREEN}[*] Inbound link: {href}{RESET}")
             urls.add(href)
             inbound_urls.add(href)
-            
-        for img_tag in soup.findAll('img'):  #check for link inside images
+
+        for img_tag in soup.findAll('img'):  # check for link inside images
             href = img_tag.attrs.get('src')
             if href == "" or href is None:
                 continue
@@ -133,7 +128,7 @@ def main_webpage_links(url):   #Returns all URLs from the given Main Website
                     if verbosity:
                         print(f"{GRAY}[!] Outbound Image link: {href}{RESET}")
                     outbound_urls.add(href)
-                    social_domain=str(urlparse(href).netloc)
+                    social_domain = str(urlparse(href).netloc)
                     if social_domain and social_domain.strip('www.') in social_list:
                         social_urls.append(href)
                 continue
@@ -158,154 +153,160 @@ def crawl(url):
     except KeyboardInterrupt:
         print(f"{RED} Keyboard Interrupt detected{RESET} ")
         sys.exit()
-    except:
-        print(f"{RED} ERROR {RESET} ")
+    except Exception as e:
+        print(f"{RED} ERROR While Crawling {RESET}\n{e} ")
         sys.exit()
+
 
 def status_check(url):
     try:
-        r=requests.get(url,headers=headers,verify=to_verify_ssl_cert)
-        if r.status_code==404:
-            number_of_broken_link+=1
+        r = requests.get(url, headers=headers, verify=to_verify_ssl_cert)
+        if r.status_code == 404:
+            global number_of_broken_link
+            number_of_broken_link += 1
             print(f"{RED}[!] BROKEN LINK: {url}{RESET}")
-        if r.status_code==301 or r.status_code==302 or r.status_code==401 or r.status_code==403 or r.status_code==429 or r.status_code==500 or r.status_code==503:
+        if r.status_code == 301 or r.status_code == 302 or r.status_code == 401 or r.status_code == 403 or r.status_code == 429 or r.status_code == 500 or r.status_code == 503:
             print(f"{CYAN}[!] HTTP ERROR : {url}{r.status_code}{RESET}")
     except KeyboardInterrupt:
         print(f"{RED} Keyboard Interrupt detected{RESET} ")
         sys.exit()
-    except:
-         print(f"{CYAN}[!] UNABLE TO CONNECT : {url}{RESET}")
+    except Exception as e:
+        print(f"{CYAN}[!] UNABLE TO CONNECT : {url}{RESET}\n{e}")
+
 
 def main_proc(deep):
-        if deep>3 or deep<=0:
-                print(f"{RED}[*] Incorrect Value for Deepness :{RESET}",deep)
-                print(f"{RED}[*] Deepness Level Varies from 1-3{RESET}")
-                print("")
-        
-        if deep==1:
-            links = main_webpage_links(url)
-            print("")
-            search_msg()
-            for link in outbound_urls:
-                link=str(link)
-                status_check(link)
-            status_check_msg()
-        elif deep==2:
-            links = main_webpage_links(url)
-            for link in links:
-                main_webpage_links(link)
-            print("")
-            search_msg()
-            for link in outbound_urls:
-                link=str(link)
-                status_check(link)
-            status_check_msg()
-            
-        elif deep==3:
-            crawl(url)
-            search_msg()
-            for link in outbound_urls:
-                link=str(link)
-                status_check(link)
-            status_check_msg()
+    if deep > 3 or deep <= 0:
+        print(f"{RED}[*] Incorrect Value for Deepness :{RESET}", deep)
+        print(f"{RED}[*] Deepness Level Varies from 1-3{RESET}")
+        print("")
 
+    if deep == 1:
+        links = main_webpage_links(url)
+        print("")
+        search_msg()
+        for link in outbound_urls:
+            link = str(link)
+            status_check(link)
+        status_check_msg()
+    elif deep == 2:
+        links = main_webpage_links(url)
+        for link in links:
+            main_webpage_links(link)
+        print("")
+        search_msg()
+        for link in outbound_urls:
+            link = str(link)
+            status_check(link)
+        status_check_msg()
+
+    elif deep == 3:
+        crawl(url)
+        search_msg()
+        for link in outbound_urls:
+            link = str(link)
+            status_check(link)
+        status_check_msg()
 
 
 def info():
-        print("")
-        print(f"{BLUE} Domain Name -->{RESET}",domain_name)
-        print(f"{BLUE} Deepness --> {RESET}",deep)
-        print(f"{BLUE} Output To a File --> {RESET}",output)
-        print(f"{BLUE} Output File --> {RESET}",output_location,"(DEFAULT)")
-        print(f"{BLUE} Verbosity --> {RESET}",verbosity)
-        print(f"{BLUE} TO verify SSL Certificate --> {RESET}",to_verify_ssl_cert)
-        print("")
+    print("")
+    print(f"{BLUE} Domain Name -->{RESET}", domain_name)
+    print(f"{BLUE} Deepness --> {RESET}", deep)
+    print(f"{BLUE} Output To a File --> {RESET}", output)
+    print(f"{BLUE} Output File --> {RESET}", output_location, "(DEFAULT)")
+    print(f"{BLUE} Verbosity --> {RESET}", verbosity)
+    print(f"{BLUE} TO verify SSL Certificate --> {RESET}", to_verify_ssl_cert)
+    print("")
+
 
 def print_output():
-    print(f"{BLUE} Printing Output To File --> ",output_location)
+    print(f"{BLUE} Printing Output To File --> ", output_location)
     with open(f"{domain_name}_links.txt", "a") as f:
-                print(" Domain Name -->",domain_name, file=f)
-                print( "Deepness -->",deep, file=f)
-                print("Output File -->",output_location,"(DEFAULT)", file=f)
-                print("Verbosity -->",verbosity, file=f)
-                print("Output To a File -->",output, file=f)
-                print("", file=f)
-                print("---Inbound URL's---", file=f)
-                print("", file=f)
-                for internal_link in inbound_urls:
-                    print(internal_link.strip(), file=f)
-                print("", file=f)
-                print("---Outbound URL's---", file=f)
-                print("", file=f)
-                for external_link in outbound_urls:
-                    print(external_link.strip(), file=f)
-                print("", file=f)
-                print("---Social URL's---", file=f)
-                print("", file=f)   
-                for social_links in social_urls:
-                    print(social_links.strip(), file=f)
-                print("", file=f)
-                print("[+] Total Inbound links:", len(inbound_urls), file=f)
-                print("[+] Total Outbound links:", len(outbound_urls), file=f)
-                print("[+] Total URLs:", len(outbound_urls) + len(inbound_urls), file=f)
-                print("", file=f)
+        print(" Domain Name -->", domain_name, file=f)
+        print("Deepness -->", deep, file=f)
+        print("Output File -->", output_location, "(DEFAULT)", file=f)
+        print("Verbosity -->", verbosity, file=f)
+        print("Output To a File -->", output, file=f)
+        print("", file=f)
+        print("---Inbound URL's---", file=f)
+        print("", file=f)
+        for internal_link in inbound_urls:
+            print(internal_link.strip(), file=f)
+        print("", file=f)
+        print("---Outbound URL's---", file=f)
+        print("", file=f)
+        for external_link in outbound_urls:
+            print(external_link.strip(), file=f)
+        print("", file=f)
+        print("---Social URL's---", file=f)
+        print("", file=f)
+        for social_links in social_urls:
+            print(social_links.strip(), file=f)
+        print("", file=f)
+        print("[+] Total Inbound links:", len(inbound_urls), file=f)
+        print("[+] Total Outbound links:", len(outbound_urls), file=f)
+        print("[+] Total URLs:", len(outbound_urls) + len(inbound_urls), file=f)
+        print("", file=f)
     f.close()
 
 
 def stats():
-        print("")
-        print("[+] Total Inbound links:", len(inbound_urls))
-        print("[+] Total Outbound links:", len(outbound_urls))
-        print("[+] Total URLs:", len(outbound_urls) + len(inbound_urls))
-        print("")
+    print("")
+    print("[+] Total Inbound links:", len(inbound_urls))
+    print("[+] Total Outbound links:", len(outbound_urls))
+    print("[+] Total URLs:", len(outbound_urls) + len(inbound_urls))
+    print("")
 
 
 def status_check_msg():
     try:
-        if number_of_broken_link==0:
-                    print(f"{RED}[*] NO BROKEN LINKS FOUND{RESET}")
+        if number_of_broken_link == 0:
+            print(f"{RED}[*] NO BROKEN LINKS FOUND{RESET}")
     except KeyboardInterrupt:
         print(f"{RED} Keyboard Interrupt detected{RESET} ")
         sys.exit()
 
+
 def search_msg():
-    print("-----------------------------------"f"{GREEN}Searching Broken Links{RESET}""-----------------------------------------")
+    print(
+        "-----------------------------------"f"{GREEN}Searching Broken Links{RESET}""-----------------------------------------")
     print("")
+
 
 def show_social():
     for i in social_urls:
         print(f"{GREEN}[--] Social URL's: {i}{RESET}")
 
 
-
-
-                                 #Main Section
-
-
+        # Main Section
 if __name__ == "__main__":
     try:
-        
+
         import argparse
-        parser = argparse.ArgumentParser(description="Broken Link Finder Tool with Python")
+        parser = argparse.ArgumentParser(
+            description="Broken Link Finder Tool with Python")
         parser.add_argument("url", help="The URL to extract links from.")
-        parser.add_argument("-d", "--deepness", help="Level of deepness to search.(Default=1)", default=1, type=int)
-        parser.add_argument("-o", "--output", help="Weather to save the output in a file. Default is False(Filename=domain-name_links.txt)")
-        parser.add_argument("-v", "--verbosity", help="Set the Verbosity of Program(Default=True)")
+        parser.add_argument(
+            "-d", "--deepness", help="Level of deepness to search.(Default=1)", default=1, type=int)
+        parser.add_argument(
+            "-o", "--output", help="Weather to save the output in a file. Default is False(Filename=domain-name_links.txt)")
+        parser.add_argument("-v", "--verbosity",
+                            help="Set the Verbosity of Program(Default=True)")
         args = parser.parse_args()
         url = args.url
         deep = args.deepness
         verbosity = args.verbosity
         domain_name = urlparse(url).netloc
-        output_location=domain_name+"_links.txt"
+        output_location = domain_name+"_links.txt"
         banner()
-        if args.verbosity==False or args.verbosity=="F":
-            verbosity=False
+        if args.verbosity == False or args.verbosity == "F":
+            verbosity = False
         else:
-            verbosity=True
-        if args.output==True or args.output=="T":
-            output=True
+            verbosity = True
+        if args.output == True or args.output == "T":
+            output = True
         else:
-            output=False
+            output = False
         is_valid(url)
         info()
         main_proc(deep)
